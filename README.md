b5d26d6b80338f471bb584c80d1a4def0267f6dd
abbas <raed.abbas@hotmail.fr>
2021-12-30 11:08:48 +0100

    au travail

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # d1_php_intermidaire
 #commencer le devoir du php inermidiare
+1-creer le form
+2-prendre la variable externe
+3- 
--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
+//include('inc_connexion.php');
 echo "bonjour";
+echo 'git';
+//roblem;
+
+
+//si le nom de ville existe dans la base, on ajoute le user dans table user_searchs
+//
+if(isset($_POST['sybmit_form']))
+{
+    $ville_nom=$_POST['ville_nom'];
+    $user=$_POST['user'];
+    
+    $resultat=$mysqli->query('SELECT ville_id, ville_nom FROM villes WHERE ville_nom= '.$ville_nom);
+    while($row=$resultat->fetchArray())
+    {
+        $row[$ville_id['ville_id']]=$row['ville_nom'];
+        $villes=$row['ville_nom'];
+    }
+
+    
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +33,13 @@ echo "bonjour";
 </head>
 <body>
     <form action="" method="POST">
-        <p>Entrez nom de ville <input type="text" name="form_ville" vallue="valider"> </p>
+        <p>Entrez votre nom <input type="text" name="user"> </p>
+        <p>Entrez nom de ville <input type="text" name="ville_nom" > </p>
+        <p><input type="submit" name="submit_form" value="valider"></p>
     </form>
+    <?php
+       
+        //ne pas oblier la fermeture de connexion
+    ?>
 </body>
 </html>
