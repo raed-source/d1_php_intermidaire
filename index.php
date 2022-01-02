ea9d52765fd25aa02b4cdcc6f8939d7241cdfd0b
raed ABBAS <raed.abbas@hotmail.fr>
2022-01-02 15:19:25 +0100

    devoir php
    changer l'emplacement et le connexion

--- index.php
+++ index.php
@@ -1,45 +1,51 @@
 <?php
-//include('inc_connexion.php');
-echo "bonjour";
-echo 'git';
-//roblem;
+//--------------------------connexion aux base de donnees---------------------------------
+$mysqli = new mysqli('localhost', 'root', '', 'projet-villes');
 
 
-//si le nom de ville existe dans la base, on ajoute le user dans table user_searchs
-//
-if(isset($_POST['sybmit_form']))
-{
-    $ville_nom=$_POST['ville_nom'];
-    $user=$_POST['user'];
-    
-    $resultat=$mysqli->query('SELECT ville_id, ville_nom FROM villes WHERE ville_nom= '.$ville_nom);
-    while($row=$resultat->fetchArray())
-    {
-        $row[$ville_id['ville_id']]=$row['ville_nom'];
-        $villes=$row['ville_nom'];
-    }
-
-    
-}
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ville_Users</title>
 </head>
+
 <body>
+    <?php
+    //------------------------si le nom de ville existe dans la base, on ajoute le user dans table user_searchs--------------------------
+
+    // if (isset($_POST['submit_form'])) 
+    // {
+    $ville_nom = $_POST['ville_nom'];
+    $user = $_POST['user'];
+
+    $resultat = $mysqli->query('SELECT ville_id, ville_nom FROM villes  ville_nom');
+    while ($row = $resultat->fetch_array()) {
+        $villes[$row['ville_id']] = $row['ville_nom'];
+    }
+
+    //}
+    ?>
     <form action="" method="POST">
         <p>Entrez votre nom <input type="text" name="user"> </p>
-        <p>Entrez nom de ville <input type="text" name="ville_nom" > </p>
+        <p>Entrez nom de ville <input type="text" name="ville_nom"> </p>
         <p><input type="submit" name="submit_form" value="valider"></p>
     </form>
-    <?php
-       
-        //ne pas oblier la fermeture de connexion
-    ?>
+    <ul>
+        <?php foreach ($villes as $id => $ville) : ?>
+            <li> <a href="index.php?id=<?php echo $id ?>"><?php echo $ville ?></a></li>
+        <?php endforeach ?>
+    </ul>
+
+    <?php $resultat->free() ?>
+    <?php $mysqli->close() ?>
+
+
 </body>
+
 </html>
