2e97961568e2ea9d18688edac5d4fc51b026072d
raed ABBAS <raed.abbas@hotmail.fr>
2022-01-28 01:33:53 +0100

    modif
    modifié mais ne marche pas

--- index.php
+++ index.php
@@ -24,26 +24,43 @@ $mysqli = new mysqli('localhost', 'root', '', 'projet-villes');
 
 
         if (!empty($ville)) {
-            $resultat = $mysqli->query('SELECT count(*) as ville_fois, ville_id  FROM villes WHERE ville_nom= "' . $ville . '"');
+            $resultat = $mysqli->query('SELECT count(*) as fois, ville_id, ville_nom  FROM villes WHERE ville_nom= "' . $ville . '"');
             $row = $resultat->fetch_array();
-            $count = $row['ville_fois'];
+            $count = $row['fois'];
             $db_ville_id = $row['ville_id'];
+            $db_ville_nom=$row['ville_nom'];
             //----------------------verifier si la ville existe dans la base ici la table ville--------------------------------------
             if ($count > 0) {
                 //---------------------la ville est dans la base alors creer un cookie pour compter les nombre de visite-------------------------------
-
+                //----------------ce cookie contient dex valeur le nom de la ville , et id de user ou user_id dans la base--------------
                 if (isset($_COOKIE['visite'])) {
                     $cookie_value = $_COOKIE['visite'];
                     //--------------------valeur de cookie---------------------------------------------------------------------
                     $cookie_value = unserialize($cookie_value);
-                    $user_id = $cookie_value['user_id'];
-                    $ville_id = $cookie_value[$db_ville_id];
-                    $user_searche_data = serialize($cookie_value);
+                    $web_user_id = $cookie_value['web_user_id'];
+                    $web_ville_id = $cookie_value['web_ville_id'];
+                    $web_nb_visite=$cookie_value['web_nb_visite'];
+                    $web_nb_visite++;
+                    $searche_data = serialize($cookie_value);
                     //-------------------insertion dans la table user_searche-------------------------------------------------
-                    $mysqli->query('INSERT INTO user_search (user_id, ville_id)values("' . $user_id . '", "' . $ville_id . '"');
+                    $mysqli->query('UPDATE user_search SET  nb_visite="'.$web_nb_visite.'" WHERE user_id="'.$web_user_id.'"');
+                    $message='bonjout c\'est votre '.$web_nb_visite; 
                 } else {
-                    
+                    //-------le cookie n'existe pas creer un cookie visite----------------------------
+                    $web_user_id= uniqid();
+                    $web_ville_id=$db_ville_id;
+                    $web_nb_visite=1;
+                    $searche_data['web_user_id']=$web_user_id;
+                    $searche_data['web_ville_id']=$web_ville_id;
+                    $searche_data['web_nb_visite']=$web_nb_visite;
+                    $searche_data=serialize($searche_data);
+                    //------------ajouter les informations dans la base de donnee-----------------------
+                    $mysqli->query('INSERT INTO user_search (user_id,ville_id,nb_visite)VALUES("'.$web_user_id.'","'.$web_ville_id.'","'.$web_nb_visite.'")');
+
                 }
+                setcookie('visite',$searche_data,time()+ 259200);
+                $message='bonjour c\'votre visite le '.$web_nb_visite;
+
             }
         }
     }
@@ -51,12 +68,14 @@ $mysqli = new mysqli('localhost', 'root', '', 'projet-villes');
     ?>
     <!-- ------------------------------------------------forme chercher ville---------------------------------------------- -->
     <form action="index.php" method="POST">
-        <p>Entrez votre nom <input type="text" name="user"> </p>
         <p>Entrez nom de ville <input type="text" name="ville"> </p>
         <p><input type="submit" name="submit_form" value="valider"></p>
     </form>
     <!-- --------------------------------------------------liberation des variables--------------------------------------------------------- -->
-    <?php echo $message ?>
+    <?php if(isset($message)): ?>
+    <p><?php echo $message?></p>
+    <?php endif ?>
+    <?php $resultat->free();?>
     <?php $mysqli->close(); ?>
 
 
