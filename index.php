c924757bd6f6b3395a1abac219967689635de611
abbas <raed.abbas@hotmail.fr>
2022-01-31 17:31:24 +0100

    pas de cookie
    utiliser la table user du cours

--- index.php
+++ index.php
@@ -20,62 +20,46 @@ $mysqli = new mysqli('localhost', 'root', '', 'projet-villes');
     //------------------------recuperer les variables externes--------------------------
 
     if (isset($_POST['submit_form'])) {
-        $ville = $_POST['ville']; // à remplacer par ville_id
+        $web_ville_nom = $_POST['web_ville_nom']; // à remplacer par ville_id
 
 
         if (!empty($ville)) {
-            $resultat = $mysqli->query('SELECT count(*) as fois, ville_id, ville_nom  FROM villes WHERE ville_nom= "' . $ville . '"');
+            $resultat = $mysqli->query('SELECT count(*) FROM villes WHERE ville_nom= "' . $web_ville_nom . '"');
             $row = $resultat->fetch_array();
-            $count = $row['fois'];
-            $db_ville_id = $row['ville_id'];
-            $db_ville_nom=$row['ville_nom'];
+            $count = $row[0];
+
             //----------------------verifier si la ville existe dans la base ici la table ville--------------------------------------
-            if ($count > 0) {
+            if ($count <= 0) {
+                $message = '<p> La ville saisie n\'existe pas saisissez une autre choix!</p<';
                 //---------------------la ville est dans la base alors creer un cookie pour compter les nombre de visite-------------------------------
-                //----------------ce cookie contient dex valeur le nom de la ville , et id de user ou user_id dans la base--------------
-                if (isset($_COOKIE['visite'])) {
-                    $cookie_value = $_COOKIE['visite'];
-                    //--------------------valeur de cookie---------------------------------------------------------------------
-                    $cookie_value = unserialize($cookie_value);
-                    $web_user_id = $cookie_value['web_user_id'];
-                    $web_ville_id = $cookie_value['web_ville_id'];
-                    $web_nb_visite=$cookie_value['web_nb_visite'];
-                    $web_nb_visite++;
-                    $searche_data = serialize($cookie_value);
-                    //-------------------insertion dans la table user_searche-------------------------------------------------
-                    $mysqli->query('UPDATE user_search SET  nb_visite="'.$web_nb_visite.'" WHERE user_id="'.$web_user_id.'"');
-                    $message='bonjout c\'est votre '.$web_nb_visite; 
-                } else {
-                    //-------le cookie n'existe pas creer un cookie visite----------------------------
-                    $web_user_id= uniqid();
-                    $web_ville_id=$db_ville_id;
-                    $web_nb_visite=1;
-                    $searche_data['web_user_id']=$web_user_id;
-                    $searche_data['web_ville_id']=$web_ville_id;
-                    $searche_data['web_nb_visite']=$web_nb_visite;
-                    $searche_data=serialize($searche_data);
-                    //------------ajouter les informations dans la base de donnee-----------------------
-                    $mysqli->query('INSERT INTO user_search (user_id,ville_id,nb_visite)VALUES("'.$web_user_id.'","'.$web_ville_id.'","'.$web_nb_visite.'")');
 
-                }
-                setcookie('visite',$searche_data,time()+ 259200);
-                $message='bonjour c\'votre visite le '.$web_nb_visite;
+            } else {
+                //-------la ville est dans la base----------------------------
+                $resultat = $mysqli->query('SELECT ville_id , ville_nom FROM villes WHERE ville_nom= "' . $web_ville_nom . '"');
+                $row = $resultat->fetch_array();
+                $db_ville_id = $row['ville_id'];
+                $db_ville_nom = $row['ville_nom'];
+                // $db_ville_texte=$row['ville_texte'];
+                $web_user_id = $_SESSION['user_id'];
 
+                //------------ajouter les informations dans la base de donnee-----------------------
+                $mysqli->query('INSERT INTO user_search (user_id,ville_id)VALUES("' . $web_user_id . '","' . $ville_id . '","' . $web_nb_visite . '")');
             }
         }
     }
 
+
     ?>
     <!-- ------------------------------------------------forme chercher ville---------------------------------------------- -->
     <form action="index.php" method="POST">
-        <p>Entrez nom de ville <input type="text" name="ville"> </p>
+        <p>Entrez nom de ville <input type="text" name="web_ville_nom"> </p>
         <p><input type="submit" name="submit_form" value="valider"></p>
     </form>
     <!-- --------------------------------------------------liberation des variables--------------------------------------------------------- -->
-    <?php if(isset($message)): ?>
-    <p><?php echo $message?></p>
+    <?php if (isset($message)) : ?>
+        <p><?php echo $message ?></p>
     <?php endif ?>
-    <?php $resultat->free();?>
+    <?php $resultat->free(); ?>
     <?php $mysqli->close(); ?>
 
 
